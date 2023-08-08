35b0bf8526384a0bcbb76bc53e5610b8e868c020
jeff wang <jeffwang93@gmail.com>
2023-08-08 12:43:49 -0700

    challenge 7

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +14,20 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrdersWithTagView(generics.ListCreateAPIView):
+    """
+    example call: /orders/with-tag/?tag=Dubbing
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request):
+        tag = request.query_params.get('tag', None)
+
+        order_tag = OrderTag.objects.get(name=tag)
+
+        orders = self.queryset.filter(tags=order_tag)
+        serializer = self.serializer_class(orders, many=True)
+        return Response(serializer.data, status=200)
