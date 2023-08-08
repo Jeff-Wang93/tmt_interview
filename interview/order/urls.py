21a59dac81136843f8cec590c341b41ad5134fc2
jeff wang <jeffwang93@gmail.com>
2023-08-08 12:34:57 -0700

    challenge 6

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderGetTagsView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('get-tags/', OrderGetTagsView.as_view(), name='order-get-tags'),
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +14,20 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderGetTagsView(generics.ListCreateAPIView):
+    """
+    example call: /orders/get-tags?id=1
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderTagSerializer
+
+    def get(self, request):
+        order_number = request.query_params.get('id', None)
+        order = self.queryset.get(id=order_number)
+        order_tags = order.tags.all()
+
+        serializer = self.serializer_class(order_tags, many=True)
+
+        return Response(serializer.data, status=200)
